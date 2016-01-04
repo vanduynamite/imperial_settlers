e8dd3dd5291537db879b92405c7db56b5be3ee86
vanduynamite <paul.vanduyn@gmail.com>
2016-01-03 22:12:48 -0500

    very initial stuff

--- game.py
+++ game.py
@@ -1,1 +1,17 @@
 # Where the game is run
+
+
+class Game(object):
+	def __init__(self):
+		self.num_players = 0
+		self.num_rounds = 5
+		self.players = []
+
+		self.round = 0
+
+
+	def run_game(self, players):
+
+		self.players = players
+
+		print self.players
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from game import *
+
 # Main function of Paul's Imperial Settlers simulator
+
+def main():
+	
+	game = Game()
+
+	players = {
+	'Paul': 'type',
+	'Audrey': 'type',
+	}
+
+	game.run_game(players)
+
+if __name__ == "__main__":
+    main()
